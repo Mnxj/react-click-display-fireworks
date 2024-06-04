6463bfc2cbf3f5579087ffad345cd5c58fc9740d
tianhang <tianhang.shi@thoughtworks.com>
2024-06-04 15:41:52 +0800

    first commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
-import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.js',
   output: [
     {
-      file: 'dist/bundle.cjs',
-      format: 'cjs'
-    },
-    {
-      file: 'dist/bundle.esm',
+      file: 'dist/bundle.esm.js',
       format: 'esm'
     }
   ],
   plugins: [
-    typescript({
-      tsconfig: './tsconfig.json'
-    })
-  ]
+    babel(),
+    postcss(),
+    resolve(),
+    commonjs(),
+    terser()
+  ],
+  external: ['react'],
 };
